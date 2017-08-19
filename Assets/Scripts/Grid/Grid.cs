3eedabd37e1e6c31d27d3d63138c4dc1b1bdfb49
David Tran <dt_dbqt@hotmail.com>
2017-08-18 23:11:05 -0400

    added initial grid system

---
+++ Assets/Scripts/Grid/Grid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    public GameObject CellPrefab;
+    public int SizeX = 10, SizeY = 10;
+
+    public GridCell[,] internalGrid;
+
+	// Use this for initialization
+	void Start () {
+		this.internalGrid = new GridCell[SizeX, SizeY];
+
+        for(int i = 0; i < SizeX; ++i) {
+            for(int j = 0; j < SizeY; ++j) {
+                this.internalGrid[i, j] = new GridCell();
+                // TODO: Random this.
+                this.internalGrid[i, j].Cell = Instantiate(CellPrefab, new Vector3(i, 0f, j), Quaternion.identity) as GameObject;
+                this.internalGrid[i, j].Initialize(GridCell.TerrainType.Soil, 1, 3);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Grid/GridCell.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCell : MonoBehaviour {
+
+    public enum TerrainType { Soil, Rock, Cactus }
+
+    public GameObject Cell;
+
+    public bool IsHidden = true;
+    public TerrainType Type = TerrainType.Soil;
+    public int Gold = 0;
+
+    private int HP = 3;
+
+    // Initialize with terrain, starting amount of gold, starting hp and whether it starts hidden.
+	public void Initialize(TerrainType terrain, int startingGold, int startingHP, bool startHidden = true) {
+        this.Type = terrain;
+        this.Gold = startingGold;
+        this.HP = startingHP;
+        this.IsHidden = startHidden;
+    }
+
+    // Returns the number of gold dug.
+    public int Dig() {
+        this.HP--;
+
+        if (this.HP <= 0) {
+            int goldDug = this.Gold;
+            this.Gold = 0;
+            this.IsHidden = false;
+            return goldDug;
+        }
+
+        return 0;
+    }
+}
