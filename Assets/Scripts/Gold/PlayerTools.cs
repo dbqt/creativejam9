1c51dd3c2cb8387bacb0508ddf3329079616d7b6
unknown <nadjam@337-B7.nad.qc.ca>
2017-08-19 14:58:40 -0400

    interraction entre playerTools, playerGold et shop

---
+++ Assets/Scripts/Gold/PlayerTools.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTools : MonoBehaviour
+{
+    public PickAxe pickAxe;
+    public Shield shield;
+    public Shovel shovel;
+    public TNT tnt;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
