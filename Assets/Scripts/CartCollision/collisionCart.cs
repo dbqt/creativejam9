202e8c32aa7db6bde1db16abc69d5b4bc9892271
Brandon Chan <brad_1800@hotmail.com>
2017-08-19 17:46:52 -0400

    Added Player-Cart Collision Script

---
+++ Assets/Scripts/CartCollision/collisionCart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionCart : MonoBehaviour {
+
+    private bool hasCollided;
+	// Update is called once per frame
+	void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Player1")
+        {
+            collider.gameObject.GetComponent<PlayerGold>().DepositGold();//cash money stashed
+            Debug.Log("Player 1 has stashed his gold!");
+        }
+	}
+}
---
+++ Assets/Scripts/CartCollision/collisionCart2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionCart2 : MonoBehaviour
+{
+    private bool hasCollided;
+    // Update is called once per frame
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Player2")
+        {
+            collider.gameObject.GetComponent<PlayerGold>().DepositGold();//cash money stashed
+            Debug.Log("Player 2 has stashed his gold!");
+        }
+    }
+}
