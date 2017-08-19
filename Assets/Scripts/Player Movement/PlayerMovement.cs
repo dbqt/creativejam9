dacce4dcd38b276a7c1a5bcec67e2a6a8b82b530
Brandon Chan <brad_1800@hotmail.com>
2017-08-19 15:08:48 -0400

    Added player movement scripts for player 1 and player 2

---
+++ Assets/Scripts/Player Movement/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Transform player;
+    public float translationSpeed = 0.01f;
+
+    void FixedUpdate()
+    {
+
+        float verticalValue = Input.GetAxis("Vertical_Player1");
+        float horizontalValue = Input.GetAxis("Horizontal_Player1");
+        Vector3 target = new Vector3(horizontalValue, 0, verticalValue);
+        player.Translate(target * translationSpeed, Space.World);
+    }
+}
+
+/* Input Manager Settings
+ * 
+ * 1st Axis: 
+ * Name: Horizontal_Player1
+ * [..]
+ * Type: Joystick Axis
+ * Axis: X Axis
+ * Joy Num: Joystick 1
+ * 
+ * 2nd Axis:
+ * Name: Vertical_Player1
+ * [..]
+ * Type: Joystick Axis
+ * Axis: Y Axis
+ * Joy Num: Joystick 1
+ */
---
+++ Assets/Scripts/Player Movement/PlayerMovement2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2 : MonoBehaviour {
+
+    public Transform player;
+    public float translationSpeed = 0.01f;
+	
+    void FixedUpdate()
+    {
+        float verticalValue2 = Input.GetAxis("Vertical_Player2");
+        float horizontalValue2 = Input.GetAxis("Horizontal_Player2");
+        Vector3 target = new Vector3(horizontalValue2, 0, verticalValue2);
+        player.Translate(target * translationSpeed, Space.World);
+    }
+}
+
+/* Input Manager Settings
+ * 
+ * 1st Axis: 
+ * Name: Horizontal_Player2
+ * [..]
+ * Type: Joystick Axis
+ * Axis: X Axis
+ * Joy Num: Joystick 2
+ * 
+ * 2nd Axis:
+ * Name: Vertical_Player2
+ * [..]
+ * Type: Joystick Axis
+ * Axis: Y Axis
+ * Joy Num: Joystick 2
+ */
