d8d5dfc13e4407388609792ca7e05d6f10cbd0af
Brandon Chan <brad_1800@hotmail.com>
2017-08-19 10:18:59 -0400

    Added countdown timer script

---
+++ Assets/Scripts/countdownTimer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class countdownTimer : MonoBehaviour {
+
+    public float timeLeft = 30.0f;
+    public Text timerText;
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+       
+        timeLeft -= Time.deltaTime; 
+        
+        if (timeLeft > 0.0f)
+        {
+            int timeLeftInt = (int)Convert.ToInt32(timeLeft);
+            timerText.text = timeLeftInt.ToString();
+        }
+
+        else
+            Debug.Log("Time's up");
+        
+	}
+
+}
