c3e1c83946d738f575ecb56f3e8b12ebcbab84be
unknown <nadjam@337-B7.nad.qc.ca>
2017-08-19 15:13:41 -0400

    ajout des fichiers Item.cs et Shop.cs

--- Assets/Scripts/shop/Item.cs
+++ Assets/Scripts/shop/Item.cs
@@ -7,26 +7,10 @@ public class ItemTag
 {
 
     public string name;
-    public float price;
+    public int price;
 
     public static ItemTag CreateFromJSON(string jsonString)
     {
         return JsonUtility.FromJson<ItemTag>(jsonString);
-    }
-
-    public BaseItem getGameObjectFromItem()
-    {
-        switch (name.ToLower())
-        {
-            case "bag" :    return new Bag();
-            case "shield":  return new Shield();
-            case "tnt":     return new TNT();
-            case "pickaxe": return new PickAxe();
-            case "shovel":  return new Shovel();
-
-            default:
-                Debug.Log("probleme classe ItemTag : nom " + name.ToLower() + " inexistant parmi les noms d'items");
-                return null;
-        }
     }
 }
