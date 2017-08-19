1ae174b3c434bdd6839f29cbf3d1512928558ca2
David Tran <dt_dbqt@hotmail.com>
2017-08-19 09:42:10 -0400

    added random rocks and cactis

--- Assets/Scripts/Grid/Grid.cs
+++ Assets/Scripts/Grid/Grid.cs
@@ -6,9 +6,14 @@ public class Grid : MonoBehaviour {
 
     [Header("Grid")]
     public GameObject CellPrefab;
+    public GameObject RockPrefab;
+    public GameObject CactusPrefab;
 
     public int SizeX = 10, SizeY = 10;
     public int TotalGold = 50;
+
+    public int NbRocks = 2;
+    public int NbCactus = 2;
 
     [Header("Map")]
 
@@ -19,12 +24,16 @@ public class Grid : MonoBehaviour {
 
     public GridCell[,] internalGrid;
 
+    public float TimeBeforeHidingMap = 5f;
+
     private HashSet<Coordinates> mapMarks;
+    private HashSet<Coordinates> obstacles;
 
 	// Use this for initialization
 	void Start () {
 		this.internalGrid = new GridCell[SizeX, SizeY];
         this.mapMarks = new HashSet<Coordinates>();
+        this.obstacles = new HashSet<Coordinates>();
 
         // Create grid
         for(int i = 0; i < SizeX; ++i) {
@@ -44,19 +53,49 @@ public class Grid : MonoBehaviour {
             this.internalGrid[coord.x, coord.y].Gold++;
         }
 
+        // Add Obstacles
+        for(int rock = 0; rock < this.NbRocks; rock++) {
+            var coord = new Coordinates{ x = Random.Range(0, this.SizeX), y = Random.Range(0, this.SizeY) };
+            // Make sure we get unique random coords for obstacles.
+            while (this.obstacles.Contains(coord)) {
+                coord.x = Random.Range(0, this.SizeX);
+                coord.y = Random.Range(0, this.SizeY);
+            }
+            obstacles.Add(coord);
+            var newObstacle = Instantiate(this.RockPrefab);
+            newObstacle.transform.SetParent(this.gameObject.transform);
+            newObstacle.transform.localPosition = new Vector3(coord.x, 0f, coord.y);
+            this.internalGrid[coord.x, coord.y].Type = GridCell.TerrainType.Rock;
+        }
+
+        for(int cactus = 0; cactus < this.NbRocks; cactus++) {
+            var coord = new Coordinates{ x = Random.Range(0, this.SizeX), y = Random.Range(0, this.SizeY) };
+            // Make sure we get unique random coords for obstacles.
+            while (this.obstacles.Contains(coord)) {
+                coord.x = Random.Range(0, this.SizeX);
+                coord.y = Random.Range(0, this.SizeY);
+            }
+            obstacles.Add(coord);
+            var newObstacle = Instantiate(this.CactusPrefab);
+            newObstacle.transform.SetParent(this.gameObject.transform);
+            newObstacle.transform.localPosition = new Vector3(coord.x, 0f, coord.y);
+            this.internalGrid[coord.x, coord.y].Type = GridCell.TerrainType.Cactus;
+        }
+
         // Populate map
         // TODO: Place/align Map in the middle of the grid
         //float middleX = ((float)SizeX) /2f;
         //float middleY = ((float)SizeY) /2f;
 
-        foreach(Coordinates c in mapMarks) {
+        foreach(Coordinates c in this.mapMarks) {
             var mark = Instantiate(RedXPrefab) as GameObject;
-            mark.transform.SetParent(Map);
+            mark.transform.SetParent(this.Map);
             mark.transform.localPosition = new Vector3(c.x, 0f, c.y);
             mark.transform.localRotation = Quaternion.identity;
         }
 
         ShowMap();
+        Invoke("HideMap", this.TimeBeforeHidingMap);
 
 	}
 
