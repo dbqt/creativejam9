ed83872e06894764eb170d51495c5427b95e44f8
David Tran <dt_dbqt@hotmail.com>
2017-08-20 08:54:20 -0400

    added decoration + exploooooooosion

--- Assets/Scripts/shop/TNTBehavior.cs
+++ Assets/Scripts/shop/TNTBehavior.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class TNTBehavior : MonoBehaviour {
 
+    public Animator Anim;
+
     public void SetTNT(float delay) {
         Invoke("Explode", delay);
     }
 
     void Explode() {
         //play explosion animation
-
+        Anim.SetTrigger("Explode");
         Destroy(this.gameObject, 0.5f);
     }
 }
