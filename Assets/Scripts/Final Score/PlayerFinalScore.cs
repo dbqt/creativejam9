ebcd21abf3e2fd1b56494e1023479a9482e3128a
Brandon Chan <brad_1800@hotmail.com>
2017-08-20 03:05:44 -0400

    Final score

---
+++ Assets/Scripts/Final Score/PlayerFinalScore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerFinalScore : MonoBehaviour {
+
+
+
+    public Text Player1_Score;
+    public Text Player2_Score;
+	// Use this for initialization
+	void Start () {
+		Player1_Score.text = 
+	}
+}
