d8370e0f78f64ecb9b9dd073933c64a91714033e
unknown <nadjam@337-B7.nad.qc.ca>
2017-08-19 21:02:29 -0400

    adding EnvironmentSoundManager

---
+++ Assets/Scripts/EnvironmentSoundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentSoundManager : MonoBehaviour
+{
+    public AudioClip bow;
+    public AudioClip menuShopSoundtrack, gameSoundtrack;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void PlayMenuSoundtrack()
+    {
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().PlayOneShot(MenuSoundtrack);
+    }
+
+    void PlayGameSoundtrack()
+    {
+        GetComponent<AudioSource>().Stop();
+        GetComponent<AudioSource>().PlayOneShot(gameSoundtrack);
+    }
+
+    void PlayBowSoundEffect()
+    {
+        GetComponent<AudioSource>().PlayOneShot(bow);
+    }
+}
